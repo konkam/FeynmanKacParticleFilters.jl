3392629b54b29495661c50b65fd1c1b75e53c8db
konkam <guillaume.konkamking.work@gmail.com>
2018-11-12 15:56:19 +0100

    first test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,5 @@ println("Testing...")
 using Test
 using FeynmanKacParticleFilters
 @test 1 == 1
+
+include("test_generic_particle_filter_algorithm.jl")
--- test/test_generic_particle_filter_algorithm.jl
+++ test/test_generic_particle_filter_algorithm.jl
@@ -1,1 +1,6 @@
 using StatsFuns, Distributions
+
+@testset "Test ESS functions" begin
+    @test FeynmanKacParticleFilters.ESS(repeat([1], inner = 10)./10) ≈ 10 atol=10.0^(-7)
+    @test FeynmanKacParticleFilters.logESS(repeat([1], inner = 10)./10 |> v -> log.(v)) ≈ log(10) atol=10.0^(-7)
+end;
