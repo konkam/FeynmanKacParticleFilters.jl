a527f5f9aeb069031af2a44381e928d0a586b8af
konkam <guillaume.konkamking.work@gmail.com>
2018-11-12 16:17:53 +0100

    idem as previous

--- src/Cox_Ingersoll_Ross_model.jl
+++ src/Cox_Ingersoll_Ross_model.jl
@@ -1,3 +1,5 @@
+using Distributions
+
 function rCIR(n::Integer, Dt::Real, x0::Real, δ, γ, σ)
     β = γ/σ^2*exp(2*γ*Dt)/(exp(2*γ*Dt)-1)
     if n == 1
@@ -33,4 +35,20 @@ function create_log_potential_functions_CIR(data)
         return x -> sum(logpdf.(Poisson(x), y))
     end
     return create_potential_functions(data, potential)
+end
+
+transition_CIR(n::Integer, Dt::Real, x0::Real, δ, γ, σ) = rCIR(n::Integer, Dt::Real, x0::Real, δ, γ, σ)
+
+function rec_transition_CIR(Dts, x, δ, γ, σ)
+    x_new = transition_CIR(1, Dts[1], x[end], δ, γ, σ)
+    if length(Dts) == 1
+        return Float64[x; x_new]
+    else
+        return Float64[x; rec_transition_CIR(Dts[2:end], x_new, δ, γ, σ)]
+    end
+end
+
+function generate_CIR_trajectory(times, x0, δ, γ, σ)
+    Dts = diff(times)
+    return rec_transition_CIR(Dts, [x0], δ, γ, σ)
 end
--- src/FeynmanKacParticleFilters.jl
+++ src/FeynmanKacParticleFilters.jl
@@ -6,4 +6,5 @@ include("generic_particle_filter_algorithm.jl")
 include("marginal_likelihood.jl")
 include("sample_from_particle_filter.jl")
 include("Cox_Ingersoll_Ross_model.jl")
+include("common_utility_functions.jl")
 end # module
