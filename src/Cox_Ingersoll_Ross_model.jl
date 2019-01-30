b6ef3c7cf0903e7bb448a9cabc2409ca0c2c9f5b
konkam <guillaume.konkamking.work@gmail.com>
2019-01-30 19:58:36 +0100

    added density function of CIR

--- src/Cox_Ingersoll_Ross_model.jl
+++ src/Cox_Ingersoll_Ross_model.jl
@@ -1,4 +1,4 @@
-using Distributions
+using Distributions, SpecialFunctions
 
 function rCIR(n::Integer, Dt::Real, x0::Real, δ, γ, σ)
     β = γ/σ^2*exp(2*γ*Dt)/(exp(2*γ*Dt)-1)
@@ -8,7 +8,7 @@ function rCIR(n::Integer, Dt::Real, x0::Real, δ, γ, σ)
     else
         ks = rand(Poisson(γ/σ^2*x0/(exp(2*γ*Dt)-1)), n)
         return rand.(Gamma.(ks .+ δ/2, 1/β))
-    end 
+    end
 end
 
 function create_transition_kernels_CIR(data, δ, γ, σ)
@@ -52,3 +52,30 @@ function generate_CIR_trajectory(times, x0, δ, γ, σ)
     Dts = diff(times)
     return rec_transition_CIR(Dts, [x0], δ, γ, σ)
 end
+
+# Iacus, Stefano M. Simulation and inference for stochastic differential equations: with R examples. Springer Science & Business Media, 2009. p.47
+function CIR_transition_density_param_iacus_cuvq(c::Real, u::Real, v::Real, q::Real)
+    return c * exp(-(u+v)) * (u/v)^(q/2) * besseli(q, 2*sqrt(u*v))
+end
+
+function u_iacus(c::Real, x0::Real, θ2::Real, t::Real)
+    return c*x0*exp(-θ2*t)
+end
+function v_iacus(c::Real, y::Real)
+    return c*y
+end
+function q_iacus(θ1::Real, θ3::Real)
+    return 2*θ1/θ3^2-1
+end
+function c_iacus(θ2::Real, θ3::Real, t::Real)
+    return 2*θ2/(θ3^2*(1-exp(-θ2*t)))
+end
+function CIR_transition_density_param_iacus(x::Real, x0::Real, t::Real, θ1::Real, θ2::Real, θ3::Real)
+    c = c_iacus(θ2, θ3, t)
+    u = u_iacus(c, x0, θ2, t)
+    v = v_iacus(c, x)
+    q = q_iacus(θ1, θ3)
+    return CIR_transition_density_param_iacus_cuvq(c, u, v, q)
+end
+
+CIR_transition_density(x::Real, x0::Real, t::Real, δ::Real, γ::Real, σ::Real) = CIR_transition_density_param_iacus(x::Real, x0::Real, t::Real, δ*σ^2, 2*γ, 2*σ)
--- test/test_CIR_functions.jl
+++ test/test_CIR_functions.jl
@@ -1,1 +1,3 @@
 @test_nowarn FeynmanKacParticleFilters.rCIR(10, 0.5, 0.2, 1., 0.5, 1.2)
+
+@test_nowarn FeynmanKacParticleFilters.CIR_transition_density(0.2, 0.5, 1.1, 1.2, 0.3, 0.6)
