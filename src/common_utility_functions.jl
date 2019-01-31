096c2b548a43731ed894c3969e32c80ee41a3d3d
konkam <guillaume.konkamking.work@gmail.com>
2019-01-31 09:53:21 +0100

    dict reversal function

--- src/common_utility_functions.jl
+++ src/common_utility_functions.jl
@@ -11,3 +11,9 @@ function reverse_time_in_dict(dict)
     times = dict |> keys |> collect |> sort
     return Dict(zip(reverse(times), (dict[k] for k in times)))
 end
+
+function reverse_time_in_dict_except_first(dict)
+    times = dict |> keys |> collect |> sort
+    revtimes = reverse(times[2:end])
+    return merge(Dict(times[1] => dict[times[1]]), Dict(zip(revtimes, (dict[t] for t in times[2:end]))))
+end
