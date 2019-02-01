29087e48ac9282fad9f1afb28521800bfbbddeb9
konkam <guillaume.konkamking.work@gmail.com>
2019-02-01 19:32:45 +0100

    added testfile

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,7 @@ using FeynmanKacParticleFilters
 
 @time include("test_generic_particle_filter_algorithm.jl")
 @time include("test_common_utility_functions.jl")
+@time include("test_create_transition_kernels.jl")
 @time include("test_marginal_likelihood.jl")
 @time include("test_sample_from_particle_filter.jl")
 @time include("test_CIR_functions.jl")
